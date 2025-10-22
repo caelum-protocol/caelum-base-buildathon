86e98e93df2f1096f68b4ca505caa0537dee19c9
Caelum Protocol <Founder@caelumprotocol.org>
2025-10-22 18:00:23 -0500

    Moved discord information to front end

--- frontend/.next/server/middleware-build-manifest.js
+++ frontend/.next/server/middleware-build-manifest.js
@@ -20,11 +20,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
-    "/queue": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/queue.js"
-    ],
     "/whispers": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
